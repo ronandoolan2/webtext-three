ad745d25e7fd86d7fa4b64297adcf49b6d1c178c
Ronan Doolan <ronan.doolan2@mail.dcu.ie>
2017-03-20 19:17:30 +0000

    Add files via upload

---
+++ open_threeie.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import time
+#driver = webdriver.Firefox()
+baseurl = "https://login.three.ie/"
+
+username = ""
+password = ''
+
+xpaths = { 'usernameTxtBox' : ".//*[@id='username']",
+           'passwordTxtBox' : ".//*[@id='password']",
+           'submitButton' :   ".//*[@id='myAccount']/form/table/tbody/tr[3]/td[2]/input[2]"
+          # 'next' : ".//*[@id='next']"
+         }
+
+mydriver = webdriver.Firefox()
+mydriver.get(baseurl)
+mydriver.maximize_window()
+
+#Clear Username TextBox if already allowed "Remember Me" 
+mydriver.find_element_by_xpath(xpaths['usernameTxtBox']).clear()
+
+#Write Username in Username TextBox
+mydriver.find_element_by_xpath(xpaths['usernameTxtBox']).send_keys(username)
+
+#Clear Password TextBox if already allowed "Remember Me" 
+#mydriver.find_element_by_xpath(xpaths['passwordTxtBox']).clear()
+
+#Write Password in password TextBox
+#mydriver.find_element_by_xpath(xpaths['passwordTxtBox']).send_keys(password)
+
+#Click Login button
+#mydriver.find_element_by_xpath(xpaths['next']).click()
+
+#import time
+#time.sleep(5)
+mydriver.find_element_by_xpath(xpaths['passwordTxtBox']).clear()
+
+#Write Password in password TextBox
+mydriver.find_element_by_xpath(xpaths['passwordTxtBox']).send_keys(password)
+
+
+mydriver.find_element_by_xpath(xpaths['submitButton']).click()
+
+time.sleep(5)
+mydriver.find_element_by_xpath(".//*[@id='my-3-usefulLinks-desktop']/div[2]/div/ul[1]/li[4]/a").click()
+time.sleep(5)
+mydriver.switch_to_frame(mydriver.find_element_by_xpath("//frame"))
+mydriver.find_element_by_xpath(".//*[@id='txtA_SMSMessage']").send_keys("test")
+#mydriver.switch_to_frame(mydriver.find_element_by_xpath("//frame"))
+time.sleep(2)
+mydriver.find_element_by_xpath(".//*[@id='div_RecipientInput']").send_keys("")
+
+mydriver.find_element_by_xpath("//*[@id='imgSendYourText']").click()
